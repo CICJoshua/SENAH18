593b8a3caf006345362af2ae9148b9f30b79feb4
CICJoshua <37729971+CICJoshua@users.noreply.github.com>
2018-07-05 23:16:32 -0500

    script
    script

---
+++ SEDNAH/Assets/Script/Camara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour {
+
+    public GameObject obj;
+    private float RX;
+    private float RY;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RX = Input.GetAxis("Mouse X");
+        RY = Input.GetAxis("Mouse Y");
+
+        this.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
+        transform.Rotate(-RY, RX, 0);
+    }
+}
---
+++ SEDNAH/Assets/Script/Caminado.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caminado : MonoBehaviour {
+
+
+    private float X;
+    private float Z;
+    
+    private float Speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        X = Input.GetAxis("Horizontal");
+        Z = Input.GetAxis("Vertical");
+        
+
+        transform.Translate(X*Speed, 0, Z*Speed);
+        
+	}
+}
